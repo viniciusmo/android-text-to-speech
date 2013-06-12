6e8f8300001fba8fdf7f941541f52be2efb196d5
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-06-12 14:26:46 -0300

    Adicionando alguns testes unitários

--- android-text-to-speech/src/test/java/com/viniciusmo/androidtextspeech/TranslatorTest.java
+++ android-text-to-speech/src/test/java/com/viniciusmo/androidtextspeech/TranslatorTest.java
@@ -1,13 +1,45 @@
 package com.viniciusmo.androidtextspeech;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
-public class TranslatorTest extends TestCase {
+import org.junit.Test;
 
+public class TranslatorTest {
+
+	@Test
 	public void testTranslateCadeiraForChairUsingPTBRtoEN() {
 		String expected = "chair";
 		String actual = Translator.translate(Language.PORTUGUESE_BR,
 				Language.ENGLISH, "cadeira");
 		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testTranslateChairForCadeiraUsingENtoPTBR() {
+		String expected = "chair";
+		String actual = Translator.translate(Language.PORTUGUESE_BR,
+				Language.ENGLISH, "cadeira");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testTranslateChairForCadeiraUsingPTBRtoEN() {
+		String expected = "cadeira";
+		String actual = Translator.translate(Language.ENGLISH,
+				Language.PORTUGUESE_BR, "chair");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testTranslateFailTextUsingPTBRtoEN() {
+		String expected = "asdadasdasdasdasdasd";
+		String actual = Translator.translate(Language.ENGLISH,
+				Language.PORTUGUESE_BR, "asdadasdasdasdasdasd");
+		assertEquals(expected, actual);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testTranslateEmptyString() {
+		Translator.translate(Language.ENGLISH, Language.PORTUGUESE_BR, "");
 	}
 }
