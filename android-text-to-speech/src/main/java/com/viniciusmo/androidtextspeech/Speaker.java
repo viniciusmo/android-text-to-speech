966b00baf64b946561091e36dc2f193e96f880e3
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-06-14 20:07:44 -0300

    Implementacao do speack

--- android-text-to-speech/src/main/java/com/viniciusmo/androidtextspeech/Speaker.java
+++ android-text-to-speech/src/main/java/com/viniciusmo/androidtextspeech/Speaker.java
@@ -2,10 +2,18 @@ package com.viniciusmo.androidtextspeech;
 
 import java.io.InputStream;
 
+import android.content.Context;
+
+import com.viniciusmo.androidtextspeech.player.PlayerStream;
+import com.viniciusmo.androidtextspeech.translate.TranslatorSpeech;
+
 public class Speaker {
 
-	public static void speack(String text, Language from) {
-		
+	public static void speack(String text, Language from, Context context) {
+		TranslatorSpeech translatorSpeech = new TranslatorSpeech(text, from);
+		PlayerStream playerStream = new PlayerStream(context);
+		InputStream inputStream = translatorSpeech.translate();
+		playerStream.play(inputStream);
 	}
 
 	public static InputStream getInputStream(String text, Language from) {
